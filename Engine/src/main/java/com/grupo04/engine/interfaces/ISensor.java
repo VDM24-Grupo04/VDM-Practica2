02541bd0117febbe78810c8a780436f7fc5ad450
pedroleo <pedroleo@ucm.es>
2024-12-30 08:18:58 +0100

    Comentario sobre los sensores

--- Engine/src/main/java/com/grupo04/engine/interfaces/ISensor.java
+++ Engine/src/main/java/com/grupo04/engine/interfaces/ISensor.java
@@ -1,5 +1,8 @@
 package com.grupo04.engine.interfaces;
 
+// Se ha creado una clase en el motor común que enmascara un sensor, para
+// poder usarlos en la lógica y crear mecánicas a partir de ellos.
+// Sin embargo, de momento solo existen sensores en Android
 public interface ISensor {
     enum SensorType {GYROSCOPE}
 
